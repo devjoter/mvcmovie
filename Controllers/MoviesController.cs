632ec175e82ae01064fe07fe185596bf9a455624
Marcin Wiatr <devjoter@gmail.com>
2023-06-04 22:30:22 +0200

    updat index in controller

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -20,12 +20,29 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index()
-        {
-              return _context.Movie != null ? 
-                          View(await _context.Movie.ToListAsync()) :
-                          Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+        public async Task<IActionResult> Index(string searchString)
+{
+            if (_context.Movie == null)
+            {
+                return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+            }
+
+            var movies = from m in _context.Movie
+                        select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title!.Contains(searchString));
+            }
+
+            return View(await movies.ToListAsync());
         }
+        // public async Task<IActionResult> Index()
+        // {
+        //       return _context.Movie != null ? 
+        //                   View(await _context.Movie.ToListAsync()) :
+        //                   Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+        // }
 
         // GET: Movies/Details/5
         public async Task<IActionResult> Details(int? id)
